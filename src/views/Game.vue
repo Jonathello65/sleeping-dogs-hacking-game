7c798cccb586f10c3c58bcb862a5cbe0af80e87b
Jonathan <jwjonathellow@gmail.com>
2021-01-20 22:52:27 -0500

    Intro code for input buttons

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,16 +1,18 @@
 <template>
 <div id="game">
     <Intro/>
+    <HackGame/>
 </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Intro from "@/components/Intro.vue"; // @ is an alias to /src
+import HackGame from "@/components/HackGame.vue";
 
 @Component({
     components: {
-        Intro
+        Intro, HackGame
     }
 })
 export default class Home extends Vue {}
