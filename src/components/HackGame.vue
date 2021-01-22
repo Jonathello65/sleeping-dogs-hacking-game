f3329a754e73e4790bc82d7996ce026cdd9baf71
Jonathan <jwjonathellow@gmail.com>
2021-01-21 21:39:23 -0500

    Add digit selectors

--- src/components/HackGame.vue
+++ src/components/HackGame.vue
@@ -1,92 +1,34 @@
 <template>
-    <div class="hello">
-        <button v-on:click="incrementNumber(1)">Up1</button>
-        <button v-on:click="incrementNumber(2)">Up2</button>
-        <button v-on:click="incrementNumber(3)">Up3</button>
-        <button v-on:click="incrementNumber(4)">Up4</button>
-        
-        <ul>
-            <li><h3>{{ digit1 }}</h3></li>
-            <li><h3>{{ digit2 }}</h3></li>
-            <li><h3>{{ digit3 }}</h3></li>
-            <li><h3>{{ digit4 }}</h3></li>
-        </ul>
-
-        <button v-on:click="decrementNumber(1)">Down1</button>
-        <button v-on:click="decrementNumber(2)">Down2</button>
-        <button v-on:click="decrementNumber(3)">Down3</button>
-        <button v-on:click="decrementNumber(4)">Down4</button>
+    <div class="hack-game">
+        <b-container>
+            <b-row>
+                <b-col v-for="digit in player.digits" :key="digit.ID">
+                    <DigitSelector/>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
 <script lang="ts">
 import {Component, Prop, Vue} from "vue-property-decorator";
+import DigitSelector from "@/components/DigitSelector.vue";
 
-@Component
-export default class HelloWorld extends Vue {
-    @Prop() private msg!: string;
-
-    private digit1: number = 0;
-    private digit2: number = 0;
-    private digit3: number = 0;
-    private digit4: number = 0;
-
-    private incrementNumber(digit: number): void {
-        switch (digit) {
-            case 1:
-                this.digit1++;
-                break;
-            case 2:
-                this.digit2++;
-                break;
-            case 3:
-                this.digit3++;
-                break;
-            case 4:
-                this.digit4++;
-                break;
-            default:
-                break;
-        }
-        this.loopDigits();
+@Component({
+    components: {
+        DigitSelector
     }
+})
 
-    private decrementNumber(digit: number): void {
-        switch (digit) {
-            case 1:
-                this.digit1--;
-                break;
-            case 2:
-                this.digit2--;
-                break;
-            case 3:
-                this.digit3--;
-                break;
-            case 4:
-                this.digit4--;
-                break;
-            default:
-                break;
-        }
-        this.loopDigits();
-    }
+export default class HackGame extends Vue {
+    public static readonly DIGIT_COUNT: number = 4;
+    private player = {
+        digits: [] as DigitSelector[]
+    };
 
-    private loopDigits(): void {
-        const digits = [this.digit1, this.digit2, this.digit3, this.digit4];
-
-        digits.forEach((value, index) => {
-            digits[index] = this.checkDigit(value);
-        });
-    }
-
-    private checkDigit(digit: number): number {
-        console.log(digit);
-        if (digit > 9) {
-            return 0;
-        } else if (digit < 0 ) {
-            return 9;
-        } else {
-            return digit;
+    private mounted(): void {
+        for (let i = 0; i < HackGame.DIGIT_COUNT; i++) {
+            this.player.digits.push(new DigitSelector());
         }
     }
 }
@@ -94,18 +36,5 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-    margin: 40px 0 0;
-}
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
-}
+@import "@/styles/Body.scss";
 </style>
