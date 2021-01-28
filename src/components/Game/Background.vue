04e63b77088b1da772e05994b9edbfcc76e84764
Christopher Wright <cwright814@gmail.com>
2021-01-27 23:20:04 -0500

    8: Initial work for background badge

---
+++ src/components/Game/Background.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="background">
+    <div class="badge">&zwnj;</div>
+</div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class Background extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/styles/Body.scss";
+
+.badge {
+    width: 448px;
+    height: 424px;
+    margin: 0 auto;
+    background-image: url("../../assets/image/HKPDLogoMask.webp");
+    mix-blend-mode: color-dodge;
+    // filter: blur(3px) brightness(116);
+}
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,22 +1,22 @@
 <template>
-    <div id="game">
-        <Intro/>
-        <HackGame v-if="$store.getters.isPlaying"/>
-        <Outro v-if="!$store.getters.isPlaying"/>
-    </div>
+<div id="game">
+    <Background/>
+    <Intro/>
+    <HackGame v-if="$store.getters.isPlaying"/>
+    <Outro v-if="!$store.getters.isPlaying"/>
+</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import Background from "@/components/Game/Background.vue";
 import Intro from "@/components/Game/Intro.vue";
 import HackGame from "@/components/Game/HackGame.vue";
 import Outro from "@/components/Game/Outro.vue";
 
 @Component({
     components: {
-        Intro,
-        HackGame,
-        Outro
+        Background, Intro, HackGame, Outro
     }
 })
 export default class Game extends Vue {}
