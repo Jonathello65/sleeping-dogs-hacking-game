4e51b176400ebd247ce8e7c46c2691893248eb76
Christopher Wright <cwright814@gmail.com>
2021-01-29 00:23:51 -0500

    9: Add background with animation

--- src/components/Game/Background.vue
+++ src/components/Game/Background.vue
@@ -1,30 +1,92 @@
 <template>
 <div class="background">
-    <div class="badge">&zwnj;</div>
+    <div class="bg-color"></div>
+    <div class="badge inner"></div>
+    <div class="badge outer"></div>
 </div>
 </template>
-
-
-<script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
-
-@Component
-export default class Background extends Vue {
-
-}
-</script>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "@/styles/Body.scss";
+$badge-inner-initial-scale: 0.125;
 
-.badge {
-    width: 448px;
-    height: 424px;
+.bg-color {
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 480px;
+    height: 825px;
     margin: 0 auto;
-    background-image: url("../../assets/image/HKPDLogoMask.webp");
+    background-image: url(../../assets/image/Background.webp);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+.badge {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 116px;
+    width: 336px;
+    height: 318px;
+    margin: 0 auto;
+    background-image: url(../../assets/image/HKPDLogoMask.webp);
+    background-size: contain;
+    background-repeat: no-repeat;
     mix-blend-mode: color-dodge;
-    // filter: blur(3px) brightness(116);
+    visibility: hidden;
+    &.inner {
+        animation-delay: 1s;
+        animation-duration: 1s;
+        animation-name: badge-inner-fade-in;
+        animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+        animation-fill-mode: forwards;
+        transform: scale(#{$badge-inner-initial-scale});
+        filter: brightness(0);
+    }
+    &.outer {
+        animation-delay: 0.5s;
+        animation-duration: 1.5s;
+        animation-name: badge-outer-fade-in-out;
+        animation-timing-function: cubic-bezier(0.215, 0.610, 0.355, 1);
+        animation-fill-mode: forwards;
+        filter: blur(0px) brightness(0);
+    }
+    &:empty {
+        display: block;
+    }
+}
+@keyframes badge-inner-fade-in {
+    from {
+        transform: scale(#{$badge-inner-initial-scale});
+        filter: brightness(0);
+        visibility: visible;
+    }
+    to {
+        transform: scale(1);
+        filter: brightness(1);
+        visibility: visible;
+    }
+}
+@keyframes badge-outer-fade-in-out {
+    0% {
+        filter: blur(0px) brightness(0);
+        visibility: visible;
+    }
+    22% {
+        filter: blur(1px) brightness(1.25);
+    }
+    33% {
+        filter: blur(5px) brightness(100);
+    }
+    55% {
+        filter: blur(7px) brightness(4);
+    }
+    100% {
+        filter: blur(3px) brightness(0);
+        visibility: hidden;
+    }
 }
 </style>
