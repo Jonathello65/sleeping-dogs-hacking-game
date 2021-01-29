7692d2c731ddcb979fde26b914713165e6d46536
Christopher Wright <cwright814@gmail.com>
2021-01-29 00:43:21 -0500

    9: Fix outro layering

--- src/components/Game/Outro.vue
+++ src/components/Game/Outro.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="outro-game">
-    <h1>{{ $store.getters.getCorrectCode }} </h1>
-    <h1 v-if="$store.getters.isSuccess">ACCESS GRANTED</h1>
-    <h1 v-if="$store.getters.isFailure">ACCESS DENIED</h1>
+    <span>{{ $store.getters.getCorrectCode }}</span>
+    <span v-if="$store.getters.isSuccess">ACCESS GRANTED</span>
+    <span v-if="$store.getters.isFailure">ACCESS DENIED</span>
 </div>
 </template>
 
@@ -20,4 +20,18 @@ export default class Outro extends Vue {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "@/styles/Body.scss";
+
+.outro-game {
+    position: absolute;
+    left: 0;
+    right: 0;
+    > span {
+        display: block;
+        font-size: 2.5rem;
+        margin-top: 0;
+        margin-bottom: 0.5rem;
+        font-weight: 500;
+        line-height: 1.2;
+    }
+}
 </style>
