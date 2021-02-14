b3230a33bd3290fc61673f0ccd63606ff2b121ed
Jonathan <jwjonathellow@gmail.com>
2021-02-13 21:06:31 -0500

    Fixed outro screen

--- src/components/Game/Outro.vue
+++ src/components/Game/Outro.vue
@@ -1,8 +1,12 @@
 <template>
 <div class="outro-game">
-    <span>{{ $store.getters.getCorrectCode }}</span>
-    <span v-if="$store.getters.isSuccess">ACCESS GRANTED</span>
-    <span v-if="$store.getters.isFailure">ACCESS DENIED</span>
+    <div class="correct-code">
+        <span>{{ $store.getters.getCorrectCode }}</span>
+    </div>
+    <div class="ending-message">
+        <span v-if="$store.getters.isSuccess">ACCESS<br>GRANTED</span>
+        <span v-if="$store.getters.isFailure">ACCESS<br>DENIED</span>
+    </div>
 </div>
 </template>
 
@@ -20,18 +24,39 @@ export default class Outro extends Vue {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "@/styles/Body.scss";
+@import "@/styles/Game.scss";
+@import "@/styles/Graphic.scss";
 
 .outro-game {
-    position: absolute;
-    left: 0;
-    right: 0;
-    > span {
+    @include graphic();
+    height: $game-height;
+    width: $game-width;
+    & > .correct-code {
+        top: 0;
+        bottom: 0;
+        margin: auto 0;
+        height: 6rem;
+        width: 100%;
+        position: absolute;
         display: block;
-        font-size: 2.5rem;
-        margin-top: 0;
-        margin-bottom: 0.5rem;
+        font-size: 5rem;
+        letter-spacing: 8px;
         font-weight: 500;
         line-height: 1.2;
+        transform: translateY(-70px);
+    }
+    & > .ending-message {
+        top: 0;
+        bottom: 0;
+        margin: auto 0;
+        height: 6rem;
+        width: 100%;
+        position: absolute;
+        display: block;
+        font-size: 4rem;
+        font-weight: 500;
+        line-height: 1;
+        transform: translateY(22px);
     }
 }
 </style>
