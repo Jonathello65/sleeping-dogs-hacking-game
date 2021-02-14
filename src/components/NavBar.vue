635ca5d5283222118ceba465ec040dbcc3636e28
Christopher Wright <cwright814@gmail.com>
2021-02-13 22:38:55 -0500

    20: Change the title to be relevant

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
 <b-navbar toggleable="xl" type="dark" variant="info" fixed="top">
-	<b-navbar-brand>Sleeping Dogs Hacking Game</b-navbar-brand>
+	<b-navbar-brand>Sleeping Dogs™ Hacking Game</b-navbar-brand>
 
 	<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="about">
-    <h1>Sleeping Dogs Hacking Game</h1>
+    <h1>Sleeping Dogs™ Hacking Game</h1>
     <p>App idea and development by <a href="https://github.com/jonathello65/" target="_blank">Jonathan Wright</a>. With support from <a href="https://github.com/cwright814" target="_blank">Christopher Wright</a>.</p>
 </div>
 </template>
